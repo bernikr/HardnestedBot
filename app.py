14ddfa6702ca26d883438300a494a49a349793e7
bernikr <git@berni.kr>
2024-12-18 18:19:23 +0100

    working version

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+import asyncio
+import errno
 import logging
 import os
+import re
+import subprocess
 from tempfile import NamedTemporaryFile
 
 from dotenv import load_dotenv
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import ApplicationBuilder, CallbackQueryHandler, CommandHandler, ContextTypes, MessageHandler, \
-    filters, PicklePersistence
+from telegram.constants import ParseMode
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    PicklePersistence,
+    filters,
+)
 
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-WHITELISTED_CHAT_IDS = [int(chat_id) for chat_id in os.getenv("WHITELISTED_CHAT_IDS").split(",")]
+WHITELISTED_CHAT_IDS = [int(chat_id) for chat_id in os.getenv("WHITELISTED_CHAT_IDS", "0").split(",")]
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -50,12 +62,61 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if cuid not in context.chat_data:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="No logs found for this chat; please resend file")
         return
-    msg = await context.bot.send_message(chat_id=update.effective_chat.id, text="Decoding logs for chat id " + cuid)
+    msg = await context.bot.send_message(chat_id=update.effective_chat.id, text="Decoding logs for cuid " + cuid)
     with NamedTemporaryFile(mode="w", delete=False) as f:
         for line in sorted(context.chat_data[cuid]):
             f.write(line + "\n")
+        f.flush()
         f.close()
-        await context.bot.editMessageText(text="Logs decoded for chat id " + cuid, chat_id=update.effective_chat.id, message_id=msg.message_id)
+
+        logging.info(f"Decoding logs for tag {cuid} in file {f.name}")
+        # this section uses really hacky file descriptor stuff to get the live preview working
+        # for some reason normal pipes don't work with the hardnested utility
+        mo, so = os.openpty()
+        os.set_blocking(mo, False)
+        process = subprocess.Popen(
+            # socat tricks the program into thinking it's in a tty
+            f"./HardnestedRecovery/hardnested_main {f.name}",
+            stdout=so,
+            stderr=so,
+            shell=True,
+        )
+        os.close(so)
+        cur_out = ""
+        out = []
+        while True:
+            await asyncio.sleep(0)
+            try:
+                chunk = os.read(mo, 256)
+            except BlockingIOError:
+                await asyncio.sleep(1)
+                continue
+            except OSError as e:
+                if e.errno == errno.EIO:
+                    break
+                raise
+            if not chunk:
+                break
+
+            cur_out += chunk.decode("utf-8")
+            new_msg = cur_out.rfind("[=] Hardnested attack starting...")
+            if len(cur_out) < 4000 and new_msg <= 0:  # over 4000 characters, send a new message
+                await context.bot.edit_message_text(text=f"```\n{cur_out.strip()}\n...\n```", chat_id=update.effective_chat.id, message_id=msg.message_id, parse_mode=ParseMode.MARKDOWN)
+            else:
+                final = cur_out[:min(4000, new_msg)].rsplit("\n", 1)[0]
+                cur_out = cur_out[len(final) + 1:]
+                out.append(final)
+                await context.bot.edit_message_text(text=f"```\n{final}\n```", chat_id=update.effective_chat.id, message_id=msg.message_id, parse_mode=ParseMode.MARKDOWN)
+                msg = await context.bot.send_message(text=f"```\n{cur_out.strip()}\n...\n```", chat_id=update.effective_chat.id)
+        await context.bot.edit_message_text(text=f"```\n{cur_out}\n```", chat_id=update.effective_chat.id, message_id=msg.message_id, parse_mode=ParseMode.MARKDOWN)
+        os.close(mo)
+        out.append(cur_out)
+        out = "\n".join(out)
+        keys = set(re.findall(r"Key found for UID: [0-9a-f]+, Sector: \d+, Key type: [AB]: ([0-9a-f]+)", out))
+        logging.info(f"Found keys: {keys}")
+        if keys:
+            await context.bot.send_message(text=f"Found keys:\n```\n{"\n".join(k.upper() for k in keys)}\n```", chat_id=update.effective_chat.id, parse_mode=ParseMode.MARKDOWN)
+
 
 
 if __name__ == "__main__":
